72d1f5003e9ce5a1abea1fa7218586572a6fe769
Robert E. Price, III <reprice@live.unc.edu>
2018-12-02 06:27:03 -0500

    Controls autocomplete built
    There is an input for airports and autocomplete suggestions are returned.
    There is no interactivity yet. Interactions with the map have not
    yet been implemented.

---
+++ controls.js
@@ -0,0 +1,46 @@
+let controls_autofill = function(e) {
+
+    if ($426Airports == null) {
+        setTimeout(100, controls_autofill(e));
+    }
+
+    const autocomplete = $("div#controls-autocomplete-container");
+    const text = $(e.currentTarget).val();
+    if (text === "") {
+        autocomplete.html("");
+        return;
+    }
+
+    const div = $(e.delegateTarget);
+    let out = "";
+
+    for (const ident of $426Airports.autocomplete(text)) {
+
+        out = (`${out}<div class="autocomplete"`
+            + `data-code="${ident}"><p>`
+            + `${$426Airports.get_city(ident)} `
+            + `(${$426Airports.get_code(ident)})</p></div>`
+        );
+
+    }
+
+    autocomplete.html(out);
+    autocomplete.css("width", div.css("width"));
+    autocomplete.css("left", +div.offset().left - +($("body").css("margin-left")[0]));
+    
+}
+
+$(document).ready(() => {
+
+    $("div#controls-src-container").on(
+        "input", "input#controls-airport-src", controls_autofill
+    );
+    $("div#controls-dest-container").on(
+        "input", "input#controls-airport-dest", controls_autofill
+    );
+    $("div#controls-autofill-container").on(
+        "click", "div.autofill", controls_set_airport
+    );
+
+
+});
